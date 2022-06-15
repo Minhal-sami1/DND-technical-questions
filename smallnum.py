7943cfa192d3af2a780e50aae89e296a8d6f541b
Minhal-sami1 <romeomino415@gmail.com>
2022-06-15 08:47:15 +0500

    Create smallnum.py

---
+++ smallnum.py
@@ -0,0 +1,19 @@
+# There can be many solutions to one problem, the easiest in python is
+lst = [1999, 10, 22, 49994, 33232, 32112, 1.5]
+print(min(lst))
+"""
+# The 2nd can be
+lst = [1999, 10, 22, 49994, 33232, 32112, 1.5]
+print(sorted(lst)[0])
+"""
+"""
+# The 3rd can be
+lst = [1999, 10, 22, 49994, 33232, 32112, 1.5]
+
+minimum = lst[0]
+for i in range(len(l)): 
+    if lst[i] < minimum:
+        minimum = lst[i] 
+
+print(min1)
+"""
